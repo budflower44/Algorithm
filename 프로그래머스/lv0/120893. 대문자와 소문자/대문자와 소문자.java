29b6711ad657cf686f4efc4bdd0d5ce4958079b1
budflower44 <84706498+budflower44@users.noreply.github.com>
2023-10-09 22:50:09 +0900

    [level 0] Title: 대문자와 소문자, Time: 13.51 ms, Memory: 82.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        
+        for(char tmp : my_string.toCharArray()) {
+        	if(Character.isLowerCase(tmp)) {
+        		answer += Character.toUpperCase(tmp);
+        	} else {
+        		answer += Character.toLowerCase(tmp);
+        	}
+        }
+
+        
+        return answer;
+    }
+}
